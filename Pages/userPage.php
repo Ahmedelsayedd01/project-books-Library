aacda4dd24e19543a6942e142bfd2012077c6627
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-04-28 10:37:43 +0300

    newUpdate AAA

--- Pages/userPage.php
+++ Pages/userPage.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 session_start();
 if (!isset($_SESSION['user'])) {
 
   header("Location:index.php");
-}else{
-  
+} else {
+
   $fullName = $_SESSION['user'][0]['first_name'] . ' ' . ' ' . $_SESSION['user'][0]['last_name'];
 }
 ?>
@@ -21,21 +21,15 @@ if (!isset($_SESSION['user'])) {
   <!-- Icons Library  -->
   <script src="https://kit.fontawesome.com/bbda8ae88d.js" crossorigin="anonymous"></script>
   <!-- Bootstrap Library  -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
   </script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
   </script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
   </script>
   <!-- Jquery Library  -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-    integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
   <title>library books</title>
 </head>
@@ -262,6 +256,41 @@ if (!isset($_SESSION['user'])) {
     </main>
   </div>
   <script src="../Scripts/homePage.js"></script>
+  <script>
+    $(document).ready(function() {
+      $.getJSON({
+        type: "GET",
+        url: "../includes/functions/userApi.php",
+        success: function(data) {
+          console.log(data);
+          var books = data.bookAdmin;
+          $(books).each((val, ele) => {
+            console.log("val", val)
+            console.log("ele", ele)
+            var newBook = `<a href="./bookPage.php?id=${ele.id}" target="_blank">
+                            <div class="book">
+                              <div class="img_book">
+                                <img class="book_cover" src="../Assets/${ele.image_book}" alt="book" />
+                              </div>
+                              <div class="content_book">
+                                <span class="book_name">${ele.bookName}</span>
+                                <span class="book_who">:by<strong>${ele.author}</strong></span>
+                              </div>
+                            </div>
+                            <div class="overlay">
+                              <i class="fa-solid fa-eye"></i>
+                              <span class="count_view">5555</span>
+                            </div>
+                          </a>`;
+            $(".books").append(newBook);
+          })
+        },
+        error: function(data) {
+          alert("fail" + status.code);
+        },
+      });
+    })
+  </script>
 </body>
 
 </html>
